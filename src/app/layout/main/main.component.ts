a0ce59611e5c2675ccf84450bc6c471fac3d2e71
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-10-21 11:22:00 +0800

    feat(main layout): Add main layout component using ng cli

---
+++ src/app/layout/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
