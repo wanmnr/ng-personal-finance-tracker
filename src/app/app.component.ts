21a2d0ea46c31679bf79dd7503e0d7b26652082b
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-10-21 12:00:05 +0800

    feat(root component): Modify the root component to accommodate the new main layout component and the rest.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
