eabdbd8c2ade511273b25cd461e67607841dceae
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-10-22 10:42:46 +0800

    style(main layout): Implement MatCardModule from Angular Material and rearrange the UI structure.

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, MatCardModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+  ngOnInit(): void {
+    // Any initialization logic can go here
+  }
 }
