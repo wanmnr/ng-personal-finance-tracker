c7f32c42397a16cfe7fbf94240582ea76a0f0915
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-10-27 22:33:34 +0800

    style: Configure tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#your-primary-color",
+      },
+      spacing: {
+        16: "4rem", // For header height
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
-}
-
+};
