020169be39659cce3bdfdc301130c0922af82bf2
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-06 22:15:35 +0800

    feat(component): add LogoComponent with initial implementation
    Added `LogoComponent`:
    - `logo.component.ts`: Defines the component for displaying a company logo.
    - `logo.component.html`: Basic template to display the logo image.
    - `logo.component.scss`: Empty stylesheet for future logo styling.
    - `logo.component.spec.ts`: Unit test to ensure the component initializes correctly.
    
    This component serves as a reusable visual element for displaying the company's branding.

---
+++ src/app/layout/components/logo/logo.component.ts
@@ -0,0 +1,13 @@
+// logo.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss'
+})
+export class LogoComponent {
+
+}
