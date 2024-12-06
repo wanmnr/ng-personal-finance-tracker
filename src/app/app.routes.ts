5ff165cad6fe59b940928204417e6b50dbcb6899
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-06 22:26:51 +0800

    feat(routes): update application routes to include demo components
    Updated `app/app.routes.ts`:
    - Added `DemoComponent` with the route `/demo`.
    - Added `MemoizationDemonstrationComponent` with the route `/memoization-demo`.
    - Retained existing routes and wildcard redirection.
    
    These updates enhance navigation by introducing new demo pages for showcasing features and components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
+
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DemoComponent } from './demo/demo.component';
+import { MemoizationDemonstrationComponent } from './demo/memoization-demo.component';
 
 // Define your routes here
 export const routes: Routes = [
   { path: '', component: WelcomeComponent, title: 'Home' },
   { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'demo', component: DemoComponent, title: 'Demo' },
+  { path: 'memoization-demo', component: MemoizationDemonstrationComponent, title: 'Demo' },
   { path: '**', redirectTo: '' } // Wildcard route for invalid paths
 ];
