a9f67ad33454cdeb4a662414e8017c0f6a85f2e0
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-07 23:30:46 +0800

    refactor(core): update import path for MainComponent in AppComponent
    Updated the import path for `MainComponent` in the `AppComponent` to reflect the new file location:
    - Changed from `./layout/main/main.component` to `./layout/main/main1.component`.
    
    This change ensures the application uses the correct module struct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MainComponent } from './layout/main/main.component';
+import { MainComponent } from './layout/main/main1.component';
 
 @Component({
   selector: 'app-root',
