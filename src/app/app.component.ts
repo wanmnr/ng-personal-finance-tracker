aba8dd234065f0927c2e69439edc9a0b655ff3e0
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-09 21:22:28 +0800

    refactor(app): enhance AppComponent with spinner integration
    - Updated `AppComponent` to include spinner functionality using `SpinnerService`
    - Imported `NgxSpinnerModule` and `LoadingSpinnerComponent` for spinner support
    - Added `someAsyncOperation` and `loadSomething` methods to demonstrate spinner usage
    - Implemented `ngOnInit` to optionally initialize spinner during app startup
    - Improved user experience by integrating loading indicators for async operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 import { MainComponent } from './layout/main/main1.component';
+import { SpinnerService } from './shared/services/spinner.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainComponent],
+  imports: [MainComponent, NgxSpinnerModule, LoadingSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-personal-finance-tracker';
+
+  constructor(private spinnerService: SpinnerService) { }
+
+  someAsyncOperation() {
+    this.spinnerService.show();
+    // Your async operation
+    // ...
+    this.spinnerService.hide();
+  }
+
+  ngOnInit() {
+    // Optional: Show spinner when app initializes
+    // this.spinnerService.show();
+    this.loadSomething();
+  }
+
+  // Example method to use the spinner
+  loadSomething() {
+    this.spinnerService.show();
+    // Simulating an API call
+    setTimeout(() => {
+      this.spinnerService.hide();
+    }, 2000);
+  }
 }
