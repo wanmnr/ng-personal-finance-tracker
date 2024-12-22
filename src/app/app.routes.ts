d6494ede7e53d617a4af5e4687d980cec3cda7ae
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-22 22:36:10 +0800

    feat(app-route): update application routes with additional components and modular structure
    Enhanced the routing configuration to improve navigation and functionality:
    - Added new routes for additional demo components:
      - `DemoModalComponent` at `/demo`.
      - `DemoTooltipComponent` at `/demo-tooltip`.
      - `MYRCurrencyComponent` at `/demo-myr-currency`.
    - Updated the existing `/demo` route to serve `DemoModalComponent` with appropriate titles.
    - Retained lazy loading for `budget` and `dashboard` features for optimized performance.
    - Included commented-out examples for future lazy loading of `transactions` with `authGuard` integration, offering flexibility for implementation.
    
    These updates enhance the modularity and expand the feature set of the application's routing, allowing for easier navigation and scalability.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,18 @@ import { WelcomeComponent } from './shared/pages/welcome/welcome.component';
 import { AboutComponent } from './shared/pages/about/about.component';
 import { DemoComponent } from './demo/demo.component';
 import { MemoizationDemonstrationComponent } from './demo/memoization-demo.component';
+import { DemoModalComponent } from './demo/demo-modal.component';
+import { DemoTooltipComponent } from './demo/tooltip-demo.component';
+import { MYRCurrencyComponent } from './demo/myr-currency.component';
 
 // Define your routes here
 export const routes: Routes = [
   { path: '', component: WelcomeComponent, title: 'Home' },
   { path: 'about', component: AboutComponent, title: 'About' },
-  { path: 'demo', component: DemoComponent, title: 'Demo' },
+  { path: 'demo', component: DemoModalComponent, title: 'Demo Modal' },
+  { path: 'demo-modal', component: DemoComponent, title: 'Demo' },
+  { path: 'demo-tooltip', component: DemoTooltipComponent, title: 'Demo Tooltip' },
+  { path: 'demo-myr-currency', component: MYRCurrencyComponent, title: 'Demo' },
   { path: 'memoization-demo', component: MemoizationDemonstrationComponent, title: 'Demo' },
   {
     path: 'budget',
@@ -24,5 +30,21 @@ export const routes: Routes = [
       .then(m => m.DashboardComponent),
     title: 'Dashboard'
   },
+  // {
+  //   path: 'transactions',
+  //   component: TransactionsPageComponent,
+  //   canActivate: [authGuard],
+  //   title: 'Transactions'
+  // }
+
+  // Alternatively, if you want to use lazy loading:
+  /*
+  {
+    path: 'transactions',
+    loadChildren: () => import('./features/transactions/transactions.module').then(m => m.TransactionsModule),
+    canActivate: [authGuard],
+    title: 'Transactions'
+  }
+  */
   { path: '**', redirectTo: '' }
 ];
