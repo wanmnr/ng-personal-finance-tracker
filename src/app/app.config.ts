93ef927fd197006aea3e7b16f8c8ec493fd905c2
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-12-22 22:34:45 +0800

    feat(app-config): enhance app configuration with additional providers and interceptors
    Updated the `appConfig` to include new providers and interceptors for improved functionality and user experience:
    - Added `provideHttpClient` with support for `errorInterceptor` to manage HTTP error handling globally.
    - Configured `provideToastr` to enable user-friendly toast notifications with:
      - `positionClass` set to `toast-bottom-right`.
      - Duplicate toast prevention enabled.
    - Retained modular and scalable design with commented provision for additional interceptors like `cacheInterceptor`.
    - Ensured compatibility with existing features, such as animations, NgRx store, and NgxSpinnerModule.
    
    These changes enhance error reporting, user feedback, and overall application resilience while maintaining clean and organized configuration practices.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,26 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideToastr } from 'ngx-toastr';
 import { layoutFeature } from './layout/store/layout.state';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { routes } from './app.routes';
+import { errorInterceptor } from '@core/interceptors/error3.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([errorInterceptor]),
+      // withInterceptors([cacheInterceptor])
+    ),
     provideAnimationsAsync(),
+    provideToastr({      // ToastrModule configuration
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    }),
     provideStore({ [layoutFeature.name]: layoutFeature.reducer }),
     provideEffects(),
     importProvidersFrom(NgxSpinnerModule.forRoot()),
