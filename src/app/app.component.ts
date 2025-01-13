7a2abea36e8f100f82c0451234b33f2ae5ee3abf
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2025-01-14 00:56:12 +0800

    refactor(app.component.ts): update spinner component to full-screen variant
    Changes:
    - Replaced `LoadingSpinnerComponent` with `FullScreenLoadingSpinnerComponent` in `app.component.ts`.
    - Updated imports to reference the new full-screen spinner component in `app.component.ts`.
    
    Purpose:
    The change ensures a better user experience by replacing the standard spinner with a full-screen variant, providing a more comprehensive loading indicator for async operations.
    
    Key Features/Details:
    - Full-screen spinner enhances visibility during loading operations.
    - Updated component retains existing functionality for managing spinner visibility via the `SpinnerService`.
    
    Impact:
    - Improves the application's user interface during loading states.
    - No breaking changes; existing logic for showing and hiding the spinner remains unaffected.
    
    Notes:
    - The new spinner component should be tested across various screen sizes and resolutions to ensure consistent behavior.
    - Future enhancements could include adding customizable loading messages or animations to the full-screen spinner.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 // app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
+import { FullScreenLoadingSpinnerComponent } from './shared/components/loading-spinner/full-screen-loading-spinner.component.ts';
 import { MainComponent } from './layout/main/main1.component';
 import { SpinnerService } from './shared/services/spinner.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainComponent, NgxSpinnerModule, LoadingSpinnerComponent],
+  imports: [MainComponent, NgxSpinnerModule, FullScreenLoadingSpinnerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'ng-personal-finance-tracker';
 
-  constructor(private spinnerService: SpinnerService) { }
+  constructor(private spinnerService: SpinnerService) {}
 
   someAsyncOperation() {
     this.spinnerService.show();
